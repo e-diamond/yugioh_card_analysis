0d2a2a1badd1af204a9149278768e372e37693e1
e-diamond <68610675+e-diamond@users.noreply.github.com>
2021-03-27 15:49:15 +0000

    Changing routine to initialise set data to class method within Set

--- card_analysis.py
+++ card_analysis.py
@@ -11,67 +11,6 @@ def updateLocalFiles():
     JSON.url2File("https://db.ygoprodeck.com/api/v7/cardsets.php", cardsets_filename)
     JSON.url2File("https://db.ygoprodeck.com/api/v7/cardinfo.php", cards_filename)
 
-# find the index of the first set each card is printed in
-def getFirstSet(sets_printed):
-
-    # list of set names for all sets card printed in
-    print_names = [printset["set_name"] for printset in sets_printed]
-
-    # search cardsets for a printed set
-    for i, set in enumerate(cardsets):
-        if set.name in print_names:
-            # return index of first set found
-            return i
-
-    # return none if set not found (no tcg release)
-    return None
-
-# calculates average name length for each set
-def avNameLength():
-
-    for card in cards:
-
-        # get index of first print set in cardsets
-        index = getFirstSet(card["card_sets"])
-
-        # update set object
-        if index != None:
-            cardsets[index].update(len(card["name"]))
-
-    # exclude reprint only sets
-    new_card_sets = [set for set in cardsets if (not set.isReprintOnly())]
-
-    # return averages
-    return [set.getAverageLength() for set in new_card_sets]
-
-"""START"""
-# ask if user wants to update storage files
-update = input("Would you like to update storage files? (y/n) ")
-
-# update storage files
-if update == "y" or update == "Y":
-    updateLocalFiles()
-
-# read data from local files
-cardsets = JSON.readFromFile(cardsets_filename)
-cards = JSON.readFromFile(cards_filename)["data"]
-
-# remove sets with no tcg release date
-cardsets = [set for set in cardsets if "tcg_date" in set]
-
-# sort cardsets by date
-cardsets.sort(key=lambda set: set["tcg_date"])
-
-# change cardsets to object array
-cardsets = [Set(set["set_name"], set["tcg_date"]) for set in cardsets]
-
-# remove cards with no release set and remove tokens
-cards = [card for card in cards if ("card_sets" in card and card["type"] != "Token")]
-
-# get variable arrays
-averages = avNameLength()
-dates = [set.tcg_date for set in cardsets if (not set.isReprintOnly())]
-
 def plotData(x, y, xlabel, ylabel, trendline=False):
 
     if trendline:
@@ -85,4 +24,25 @@ def plotData(x, y, xlabel, ylabel, trendline=False):
     plt.ylabel(ylabel)
     plt.show()
 
-plotData(dates, averages, "Sets", "Mean length of card name (characters)", True)
+
+"""START"""
+# ask if user wants to update storage files
+update = input("Would you like to update storage files? (y/n) ")
+
+# update storage files
+if update == "y" or update == "Y":
+    updateLocalFiles()
+
+# read data from local files
+cardsets = JSON.readFromFile(cardsets_filename)
+cards = JSON.readFromFile(cards_filename)["data"]
+
+# initialise set objects
+cardsets = Set.initialise(cardsets, cards)
+
+# get variable arrays
+name_av = [set.averageNameLength() for set in cardsets]
+dates = [set.tcg_date for set in cardsets if (not set.isReprintOnly())]
+
+
+plotData(dates, name_av, "Sets", "Mean length of card name (characters)", True)
--- cardsets.py
+++ cardsets.py
@@ -6,17 +6,60 @@ class Set:
         self.name = name
         self.tcg_date = date.fromisoformat(tcg_date)
         self.card_count = 0
-        self.character_rt = 0
+        self.name_rt = 0
+        self.desc_rt = 0
 
-    def update(self, length):
+    def update(self, name_length, desc_length=0):
         self.card_count = self.card_count + 1
-        self.character_rt = self.character_rt + length
+        self.name_rt = self.name_rt + name_length
 
-    def getAverageLength(self):
-        return self.character_rt / self.card_count
+    def averageNameLength(self):
+        return self.name_rt / self.card_count
 
     def isReprintOnly(self):
         if self.card_count == 0:
             return True
         else:
             return False
+
+    # initialise set data
+    @classmethod
+    def initialise(cls, cardsets, cards):
+
+        # remove sets with no tcg release
+        cardsets = [set for set in cardsets if "tcg_date" in set]
+        # convert to object array
+        cardsets = [Set(set["set_name"], set["tcg_date"]) for set in cardsets]
+        # sort by release date
+        cardsets.sort(key=lambda set: set.tcg_date)
+
+        # remove cards not released in a set and tokens 
+        cards = [card for card in cards if ("card_sets" in card and card["type"] != "Token")]
+
+        for card in cards:
+
+            # get index of first print set in cardsets
+            index = cls.getFirstSet(cardsets, card["card_sets"])
+
+            # update set object
+            if index != None:
+                cardsets[index].update(len(card["name"]))
+
+        # exclude reprint only sets
+        return [set for set in cardsets if (not set.isReprintOnly())]
+
+    # find the index of the first set each card is printed in
+    @staticmethod
+    def getFirstSet(cardsets, sets_printed):
+
+        # list of set names for all sets card printed in
+        print_names = [printset["set_name"] for printset in sets_printed]
+
+        # search cardsets for a printed set
+        for i, set in enumerate(cardsets):
+            if set.name in print_names:
+                # return index of first set found
+                return i
+
+        # return none if set not found (no tcg release)
+        return None
